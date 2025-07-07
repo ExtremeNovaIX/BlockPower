e298f47b9ee203db91cc90e150cdeb46e987d019
ExtremeNovaIX <3177097870@qq.com>
2025-07-07 17:15:17 +0800

    完成Forge导入

---
+++ src/main/java/BlockPower/Main.java
@@ -0,0 +1,15 @@
+package BlockPower;
+
+import net.minecraftforge.fml.common.Mod;
+
+import static BlockPower.Main.MOD_ID;
+
+@Mod(MOD_ID)
+public class Main {
+
+    public static final String MOD_ID = "blockpower";
+
+    public Main() {
+
+    }
+}
---
+++ src/main/java/BlockPower/test.java
@@ -0,0 +1,4 @@
+package BlockPower;
+
+public class test {
+}
