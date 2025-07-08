2d27f8ed0f8531849fc55d5c4a2e4d259f4b08b0
ExtremeNovaIX <3177097870@qq.com>
2025-07-09 02:20:24 +0800

    自动注册KeyMapping到Events

--- src/main/java/BlockPower/ClientEvents/ClientEvents.java
+++ src/main/java/BlockPower/ClientEvents/ClientEvents.java
@@ -1,11 +1,18 @@
 package BlockPower.ClientEvents;
 
 import BlockPower.KeyBindings.KeyBindings;
+import net.minecraft.client.KeyMapping;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.swing.text.JTextComponent;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 
 import static BlockPower.Main.Main.MOD_ID;
 
@@ -13,9 +20,24 @@ import static BlockPower.Main.Main.MOD_ID;
 @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class ClientEvents {
 
+    private static final Logger LOGGER = LogManager.getLogger("ClientEvents");
+
     @SubscribeEvent
     public static void onKeyRegister(RegisterKeyMappingsEvent event) {
-        //注册按键绑定
-        event.register(KeyBindings.MINECART_RUSH);
+        //自动读取KeyBindings并注册按键绑定
+        for (Field field : KeyBindings.class.getFields()) {
+            //检查字段是否是public static final
+            if(Modifier.isStatic(field.getModifiers()) && Modifier.isPublic(field.getModifiers()) && Modifier.isFinal(field.getModifiers())) {
+                if(field.getType() == KeyMapping.class) {
+                    try {
+                        //自动注册按键绑定
+                        event.register((KeyMapping) field.get(field));
+                        LOGGER.info("Auto registered key binding: {}", field.getName());
+                    } catch (Exception e) {
+                        LOGGER.error("{}:Failed to register key binding: {}", e, field.getName());
+                    }
+                }
+            }
+        }
     }
 }
